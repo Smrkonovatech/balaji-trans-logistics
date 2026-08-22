40834a922be018bc286322268cc07138474ae584
surya <ssuryareddy2277@gmail.com>
2026-08-22 20:56:25 +0530

    form added

--- js/form.js
+++ js/form.js
@@ -5,11 +5,27 @@ $(function() {
 
     $(form).submit(function(e) {
         e.preventDefault();
+        
+        var formData = $(form).serialize();
 
-        $(formMessages).removeClass('error').addClass('success');
-        $(formMessages).text('Thank you. Your transport requirement has been noted. Our team will review the details you submitted.');
-
-        $('#name, #company, #email, #phone, #pickup, #destination, #ftype, #cargo, #city, #wight, #message').val('');
+        $.ajax({
+            type: 'POST',
+            url: 'mailer.php',
+            data: formData
+        })
+        .done(function(response) {
+            $(formMessages).removeClass('error').addClass('success');
+            $(formMessages).text(response);
+            $('#name, #company, #email, #phone, #pickup, #destination, #ftype, #cargo, #city, #wight, #message').val('');
+        })
+        .fail(function(data) {
+            $(formMessages).removeClass('success').addClass('error');
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
     });
 
 });
